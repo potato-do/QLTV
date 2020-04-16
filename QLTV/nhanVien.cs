fc839b117779f85c3986b1924c162241e7ffca49
LocNguyen310 <63465540+LocNguyen310@users.noreply.github.com>
2020-04-16 22:45:36 +0700

    Cập nhật nhân viên, thêm mới nhân viên
    Lêu lêu

---
+++ QLTV/nhanVien.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTV
+{
+    public partial class nhanVien : UserControl
+    {
+        public nhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuCards1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            themNhanVien n = new themNhanVien();
+            n.Show();
+        }
+    }
+}
