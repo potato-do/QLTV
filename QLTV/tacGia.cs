9509be9eff7a38c98a2b403945d3f6aa3d72db6d
potatol-dot <60807740+potatol-dot@users.noreply.github.com>
2020-04-16 23:28:11 +0700

    Revert "Revert "update""
    This reverts commit 0d652ccc525f9f8b9f183fe1e7982e2dcb70b793.

--- QLTV/tacGia.cs
+++ QLTV/tacGia.cs
@@ -34,6 +34,10 @@ namespace QLTV
             string query;
             query = "select * from TacGia";
             Form1.renderData(query, dataGridView1);
+
+            cbBoLoc.Items.Add("Mã tác giả");
+            cbBoLoc.Items.Add("Tên tác giả");
+            cbBoLoc.Items.Add("website");
         }
 
         private void btnReload_Click(object sender, EventArgs e)
@@ -47,6 +51,26 @@ namespace QLTV
         private void btnXoa_Click(object sender, EventArgs e)
         {
             deleteData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            updateData();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            moveDataToTextBox();
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            search();
+        }
+
+        private void cbBoLoc_Click(object sender, EventArgs e)
+        {
+            
         }
 
         //Hàm xóa tác giả khỏi database
@@ -65,5 +89,50 @@ namespace QLTV
                 MessageBox.Show("Xóa dữ liệu thành công!!!");
             }
         }
+
+        private void moveDataToTextBox()
+        {
+            txtMaTG.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtWebsite.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void updateData()
+        {
+            string query;
+            string maTG = txtMaTG.Text;
+            string tenTG = txtTen.Text;
+            string website = txtWebsite.Text;
+
+            //Update data
+            query = "Update TacGia set TenTG = N'" + tenTG + "', website = '" + website + "' where MaTG = " + maTG ;
+            Form1.executeQuery(query);
+            MessageBox.Show("Cập nhật dữ liệu thành công!!");
+        }
+
+        
+        //Hàm search theo bộ lọc
+        private void search()
+        {
+            string query;
+            string text = txtSearch.Text;
+            if(cbBoLoc.SelectedItem.ToString() == "Mã tác giả")
+            {
+                query = "Select * from TacGia where MaTG like '%" + text + "%'";
+                Form1.renderData(query, dataGridView1);
+            }
+            if (cbBoLoc.SelectedItem.ToString() == "Tên tác giả")
+            {
+                query = "Select * from TacGia where TenTG like '%" + text + "%'";
+                Form1.renderData(query, dataGridView1);
+            }
+            if (cbBoLoc.SelectedItem.ToString() == "website")
+            {
+                query = "Select * from TacGia where Website like '%" + text + "%'";
+                Form1.renderData(query, dataGridView1);
+            }
+        }
+
+        
     }
 }
